69b41ff6649dba3bcac10585aa1f6157a3028baa
RajanPandey1311 <rajanpandeygiit31078379@gmail.com>
2023-11-28 10:06:39 +0530

    first commit

---
+++ src/components/SpeechToText.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
+import "./speechToText.css";
+
+function SpeechToText() {
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  const [isCopied, setCopied] = useClipboard(transcript);
+
+  return (
+    <div className="container">
+      <h1 className="heading">SpeechRecognition</h1>
+      <h3>
+      Transforming Speech into Text seamlessly. Your Voice, Transformed into Words Instantly.
+      </h3>
+      <div className="textContainer">
+        <p>
+          {transcript}
+        </p>
+      </div>
+      <div className="btnContainer">
+        <button className="btn"
+          onClick={() => SpeechRecognition.startListening({ continuous: true })}
+        >
+          Start Listening
+        </button>
+        <button className="btn" onClick={() => SpeechRecognition.stopListening()}>
+          Stop Listening
+        </button>
+        <button className="btn" onClick={resetTranscript}>Clear Text</button>
+        <button className="btn" onClick={setCopied}>{isCopied ? "Copied! 👍" : "Copy?"}</button>
+      </div>
+    </div>
+  );
+}
+
+export default SpeechToText;
